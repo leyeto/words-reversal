351eb03abfb6b47ed377ca8539000efb536c10da
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2022-07-31 00:21:49 +0100

    Solution done

--- src/WordsReversed.java
+++ src/WordsReversed.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class WordsReversed {
+
+public class WordsReversed {
+
+    public static void main(String[] args){
+      reverseWords("The cat is meowing");
+    }
+
+    public static StringBuilder reverseWords(String words){
+        String currentInput[] =words.split(" ");
+        String reverseArray[] = new String[currentInput.length];
+    for(int i = 0; i < currentInput.length; i++){
+               reverseArray[i]=currentInput[currentInput.length-(i+1)];
+    }
+    StringBuilder reverseStings = new StringBuilder();
+
+    for(int i =0; i < reverseArray.length; i++){
+        reverseStings.append(reverseArray[i] + " ");
+    }
+
+    return reverseStings;
+    }
 }
